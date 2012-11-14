01490b51ded7f13e1dee927e337c45c88c32f561
cal2020 <escapebulk@youremail.com>
2012-11-13 20:09:50 -0800

    initial commit of rails app template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNewApp::Application.config.secret_token = 'f6851f6f8f3dd55a7c72cfe21cfd1ce0dc9232e158861a697a8f151d3bdaf55d46000d0210236d5891b9ce0158d839820f1d11094687d88ea9115183f273304e'
